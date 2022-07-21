6e8b65b72d54cbb4bb37c435757c99d23f909a4c
gabriel6181997 <gabriel6181997@gmail.com>
2022-07-21 21:57:41 +0900

    ✨ Create sort_algorithm file

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ global_search = 全探索 (beta).
 math = 論理的思考力を鍛える！文章題.
 bit_operation = ビット演算（bit演算）前編
 regular_expression = 正規表現.
+sort_algorithm = ソードアルゴリズム
 dp = 動的計画法（DP）
 different_algorithm = さまざまなアルゴリズム設計技法
---
+++ sort_algorithm.py
@@ -0,0 +1,18 @@
+### ソードアルゴリズム ###
+
+### 1-1 ###
+# バブルソート
+N = int(input())
+A = list(map(int, input().split()))
+
+for _ in range (N):
+    flag = False
+    for i in range(N-1):
+        if A[i] > A[i+1]:
+            flag = True
+            A[i], A[i+1] = A[i+1],A[i]
+
+    if flag:
+        print(*A)
+    else:
+        break
