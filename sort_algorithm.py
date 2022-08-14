b6e72b82b2b88c23ea1f4cd33b8a4f4d799965bf
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-14 21:10:14 +0900

    🐛 fix bugs of Sort algorithm Q6

--- sort_algorithm.py
+++ sort_algorithm.py
@@ -168,19 +168,21 @@ N = int(input())
 A = list(map(int,input().split()))
 
 def build_heap(v):
-    for i in range(len(A)//2, -1, -1):
-        left = v[2*k+1] if 2*k+1 < len(v) else 0
-        right = v[2*k+2] if 2*k+2 < len(v) else 0
-        if left == 0 and right == 0:
-            break
-        elif v[k] >= left and v[k] >= right:
-            break
-        elif left >= right:
-            v[k], v[2*k+1] = v[2*k+1], v[k]
-            k = 2*k+1
-        else:
-            v[k], v[2*k+2] = v[2*k+2], v[k]
-            k = 2*k+2
+    for x in range(len(v)//2, -1, -1):
+        k = x
+        while True:
+            left = v[2*k+1] if 2*k+1 < len(v) else 0
+            right = v[2*k+2] if 2*k+2 < len(v) else 0
+            if left == 0 and right == 0:
+                break
+            elif v[k] >= left and v[k] >= right:
+                break
+            elif left >= right:
+                v[k], v[2*k+1] = v[2*k+1], v[k]
+                k = 2*k+1
+            else:
+                v[k], v[2*k+2] = v[2*k+2], v[k]
+                k = 2*k+2
 
 build_heap(A)
 
