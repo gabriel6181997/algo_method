1067b9347e99caa085b78b60db25b3cd2dd60b97
gabriel6181997 <gabriel6181997@gmail.com>
2022-08-16 21:50:25 +0900

    ✨ Finish sort_algorithm Q9

--- sort_algorithm.py
+++ sort_algorithm.py
@@ -234,3 +234,26 @@ for i in range(N-1,0,-1):
 
 # 最終的な配列Aの様子を出力
 print(*A)
+
+### 1-9 ###
+# バゲットソート
+
+N = int(input())
+A = list(map(int, input().split()))
+
+X = N + 1
+num = [0] * X
+for a in A:
+    num[a] += 1
+acc = [0] * X
+for idx, count in enumerate(num):
+    if idx == 0:
+        acc[idx] = count
+    else:
+        acc[idx] = acc[idx-1] + count
+B = [0] * N
+for a in A:
+    acc[a] -= 1
+    B[acc[a]] = a
+
+print(*B)
